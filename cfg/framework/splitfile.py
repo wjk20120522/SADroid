6bac6fa626f5e9278de54c388f555a7e42815deb
wjk20120522 <jkwangbest@gmail.com>
2015-06-01 11:18:25 +0800

    split big file into small files

--- cfg/framework/splitfile.py
+++ cfg/framework/splitfile.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
+
+
+# split the big file ImplicitEdges.txt into 100 files
+
+if not os.path.exists("Implicit"):
+    os.makedirs('Implicit')
+for i in xrange(1, 103):
+    with open("Implicit/" + str(i) + ".txt", 'w') as f:
+        pass
+
+
+count = 0
+filenum = 1
+with open('../../tools/EdgeMiner/ImplicitEdges.txt') as bigfile:
+    for line in bigfile:
+        with open("Implicit/" + str(filenum) + ".txt", 'a') as smallfile:
+            smallfile.write(line)
+        count += 1
+        if count == 51255:
+            count = 0
+            filenum += 1
+
+
+
