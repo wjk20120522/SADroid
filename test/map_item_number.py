d6250579eaf124658fafee4a2f94197b1064e6cb
wjk20120522 <jkwangbest@gmail.com>
2015-12-04 10:15:36 +0800

    查看是否每个apk中都用了18个map_item类型

---
+++ test/map_item_number.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.append('../')
+from androguard.core.bytecodes import apk, dvm
+
+if __name__ == '__main__':
+    for root, dirs, files in os.walk('../input'):
+        for current_apk_file in files:
+            cwd = os.path.abspath("../input")
+            current_apk_file = cwd + os.sep + current_apk_file
+            if current_apk_file.endswith(".apk"):
+                a = apk.APK(current_apk_file)
+                if a.is_valid_apk():
+                    vm = dvm.DalvikVMFormat(a.get_dex())
+                    print 'File Name : ' + current_apk_file + " map item Number: " + str(len(vm.map_list.map_item))
+                else:
+                    print 'Invalid Apk'
+                    exit()
+
